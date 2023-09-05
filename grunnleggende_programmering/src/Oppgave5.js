5954411af2adbebf329c1b0779debf92af675aac
Vetle Fongen <vetle.fongen@gmail.com>
2023-09-05 13:38:44 +0200

    Oppgave 5 fungerer nå

--- grunnleggende_programmering/src/Oppgave5.js
+++ grunnleggende_programmering/src/Oppgave5.js
@@ -1,13 +1,13 @@
 import { useState } from "react"
 
 function HigherOrLower(){
-    const [RanNum, setRanNum] = useState(0);
+    const [RanNum, setRanNum] = useState(1);
     let ResultsArray = [25];
     let Computerguess = 25;
     let NumberOfGuesses = 1;
 
     function CalculateNumber(){
-        while (Computerguess != RanNum){
+        while (Computerguess != RanNum && Computerguess < 50 && Computerguess > 1 && RanNum != ""){
             if(Computerguess < RanNum){
                 Computerguess = Math.round(Computerguess + 25 / (2 ** NumberOfGuesses));
             }
@@ -16,17 +16,18 @@ function HigherOrLower(){
             }
             ResultsArray.push(Computerguess);
             NumberOfGuesses++;
-            //console.log(ResultsArray)
+            console.log(RanNum)
         }
+        let Result = ResultsArray.map((item, index) => <li key={index}>{item}</li>);
+        return Result;
+        
     }
 
     return(
         <>
-            <input type="number" onChange={e => setRanNum(e.target.value)} placeholder="Tall mellom 1 og 49" min="1" max="49" />
-            <button onClick={() => CalculateNumber()}>Guess</button>
-            {ResultsArray.map((items) => {
-                console.log(items);
-            })}
+            <p>Et tall mellom 1 og 50:</p>
+            <input type="number" placeholder="1" onChange={e => setRanNum(e.target.value)} min="1" max="50" />
+            <ul>{CalculateNumber()}</ul>
         </>
     )
 }
