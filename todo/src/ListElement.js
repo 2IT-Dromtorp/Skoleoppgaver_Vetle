8cf6ad57f6489f50688cd86e12165243a2e04bc3
Vetle Fongen <vetle.fongen@gmail.com>
2023-09-25 14:20:50 +0200

    todo

--- todo/src/ListElement.js
+++ todo/src/ListElement.js
@@ -1,16 +1,37 @@
+import { useState } from "react";
 import { todoTasks } from "./App";
 
 const ListElement = ({index, updateList}) => {
+
+    const [isInEditMode, setIsInEditMode] = useState(false);
+    const [name, setName] = useState(todoTasks.tasks[index].name)
+    const [description, setDescription] = useState(todoTasks.tasks[index].description)
+
     function UpdateCompleted(completed){
         todoTasks.tasks[index].isCompleted = completed;
         console.log(todoTasks);
     }
-    return(
-        <div className="listelement">
-            <input type='checkbox' defaultChecked={todoTasks.tasks[index].isCompleted} onChange={e => {UpdateCompleted(e.target.checked);updateList()}}></input>
-            <h2>{todoTasks.tasks[index].name}</h2>
-            <p>{todoTasks.tasks[index].description}</p>
-        </div>
-    )
+
+    if (isInEditMode){
+        return(
+            <div className="listElement">
+                <input type='checkbox' defaultChecked={todoTasks.tasks[index].isCompleted} onChange={e => {UpdateCompleted(e.target.checked);updateList()}}></input>
+                <input type="text" value={name} onChange={e => setName(e.target.value)} />
+                <input type="text" value={description} onChange={e => setDescription(e.target.value)} />
+                <button onClick={() => setIsInEditMode(false)}>Save</button>
+                <button>Delete</button>
+            </div>
+        )
+    } else {
+        return(
+            <div className="listElement">
+                <input type='checkbox' defaultChecked={todoTasks.tasks[index].isCompleted} onChange={e => {UpdateCompleted(e.target.checked);updateList()}}></input>
+                <h2>{name}</h2>
+                <p>{description}</p>
+                <button onClick={() => setIsInEditMode(true)}>Edit</button>
+                <button>Delete</button>
+            </div>
+        )
+    }
 }
 export default ListElement;
