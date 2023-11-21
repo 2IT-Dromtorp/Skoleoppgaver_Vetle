3cc3b1309307e871b8e111fc6b267f0a13c0f2f9
Frikk2015 <vetle.fongen@gmail.com>
2023-11-21 15:42:23 +0100

    startet prosjekt dommedag med vite, ts og tailwind

---
+++ prosjekt-dommedag/src/Popup.tsx
@@ -0,0 +1,11 @@
+function Popup(): JSX.Element {
+    return <>
+            <div className="bg-black bg-opacity-50 w-screen h-screen absolute flex justify-center align-middle z-10">
+                <div>
+                    <p>Test</p>
+                </div>
+            </div>
+        </>
+}
+
+export default Popup
