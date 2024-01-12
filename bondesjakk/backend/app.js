313b3a2fa8add1cdd4b23676dee78707beccde92
Vetle Fongen <vetle.fongen@gmail.com>
2024-01-12 12:38:59 +0100

    sockets varsle andre i rommet

--- bondesjakk/backend/app.js
+++ bondesjakk/backend/app.js
@@ -10,19 +10,17 @@ server.on("connection", (client) => {
     console.log("user connected")
 
     client.on("join room", (room) => {
-        console.log(room)
         if (server.sockets.adapter.rooms.get(room) === undefined || server.sockets.adapter.rooms.get(room).size >= 2) return
         client.join(room)
+        console.log("client joined room: " + room)
         client.emit("join lobby")
-        console.log(server.sockets.adapter.rooms.get(room).size)
     })
 
     client.on("create room", (room) => {
-        console.log(room)
         if (server.sockets.adapter.rooms.get(room) !== undefined) return
         client.join(room)
-        client.emit("join lobby")
         console.log("create room: " + room)
+        client.emit("join lobby")
     })
 
     client.on("joined room", () => {
@@ -32,9 +30,17 @@ server.on("connection", (client) => {
     client.on("send names", (name) => {
         server.to(Array.from(client.rooms)[1]).emit("names recieved", name)
     })
+    
+    client.on("send to game", () => {
+        console.log("game started")
+        server.to(Array.from(client.rooms)[1]).emit("send to game")
+    })
+
+    client.on("disconnecting", () => {
+        client.to(Array.from(client.rooms)[1]).emit("user left")
+    })
 
     client.on("disconnect", () => {
-        server.to(Array.from(client.rooms)[1]).emit("get names")
         console.log("user disconnected")
     })
 });
--- bondesjakk/frontend/src/Lobby.jsx
+++ bondesjakk/frontend/src/Lobby.jsx
@@ -11,7 +11,7 @@ function Lobby() {
 
     useEffect(() => {    
 
-        socket.emit("joined room", username)
+        socket.emit("joined room")
 
         function OnGotNames(name) {
             setNames(prevNames => [...prevNames, name])
@@ -21,15 +21,26 @@ function Lobby() {
             setNames([])
             socket.emit("send names", username)
         }
+        function OnUserLeft() {
+            socket.emit("joined room", username)
+        }
 
         socket.on("names recieved", (name) => OnGotNames(name))
+        socket.on("user left", OnUserLeft)
         socket.on("get names", OnGetNames)
+        socket.on("send to game", () => navigate("/game"))
 
         return () => {
             socket.off("names recieved", (name) => OnGotNames(name))
+            socket.off("user left", OnUserLeft)
             socket.off("get names", OnGetNames)
+            socket.off("send to game", () => navigate("/game"))
         };
     }, []);
+
+    function SendToGame(){
+        socket.emit("send to game")
+    }
 
     return (
         <>
@@ -37,7 +48,7 @@ function Lobby() {
             {names.map((name, index) => {
                 return <p key={index}>{name}</p>
             })}
-            {names.length >= 2 && <button className="bg-lime-500">Start game</button>}
+            {names.length >= 2 && <button onClick={() => SendToGame()} className="bg-lime-500">Start game</button>}
         </>
     )
 }
