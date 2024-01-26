b4bb38befd48d36072e5b5a6e5701e09ae987f49
Vetle Fongen <vetle.fongen@gmail.com>
2024-01-26 10:53:55 +0100

    en nettside som følger standarden

--- standard-eksempel/server/index.js
+++ standard-eksempel/server/index.js
@@ -4,11 +4,15 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static("build"))
 
-const port = 8080
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
     app.get("/api/get", (req, res) => {
         res.status(200).json({"message": "👌"})
+    })
+    app.get("*", (req, res) => {
+        res.sendFile("build")
     })
 })
